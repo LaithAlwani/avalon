e624327ed8003af3c06eea299a1af9433dd1bad1
laith <laithalwani@gmail.com>
2024-09-10 00:12:06 -0400

    ui updates

--- components/PlayersList.jsx
+++ components/PlayersList.jsx
@@ -1,33 +1,46 @@
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
 export default function PlayersList({ players, currentPlayer }) {
   const isVisble = (player) => {
     if (
       currentPlayer?.ability === "percival" &&
       (player?.ability === "merlin" || player?.ability === "morgana")
     )
-      return " (Merlin)";
+      return (
+        <>
+          <img
+            src="/char/merlin.webp"
+            alt="loyal"
+            style={{ width: "64px", borderRadius: "0.5rem" }}
+          />
+          <img src="/char/loyal_symbol.webp" alt="loyal" style={{ width: "80px" }} />
+        </>
+      );
     if (
       currentPlayer?.ability === "merlin" &&
       player?.role === "minion" &&
       player?.ability != "mordred" &&
       player?.ability != "oberon"
     )
-      return ` (${player.role})`;
+      return <img src="/char/minion_symbol.webp" alt="minion" style={{ width: "72px" }} />;
+
     if (
       currentPlayer?.role === "minion" &&
       currentPlayer?.ability != "oberon" &&
       player?.role === "minion" &&
       player?.ability != "oberon"
     )
-      return ` (${player.role})`;
-    else return "";
+      return <img src="/char/minion_symbol.webp" alt="minion" style={{ width: "72px" }} />;
+    // else return <img src="/char/loyal_symbol.webp" alt="loyal" style={{ width: "80px" }} />;
+    else return null;
   };
   return (
     <ul>
       {currentPlayer && (
-        <li>
-          <img src={currentPlayer?.avatar} alt="" className="avatar" /> {currentPlayer?.username}
-          {currentPlayer.role && ` (${currentPlayer.role})`}{" "}
-          {currentPlayer.ability && `(${currentPlayer?.ability})`}
+        <li
+          className="current-player">
+          <CurrentPlayer currentPlayer={currentPlayer} />
         </li>
       )}
       {players &&
@@ -35,14 +48,49 @@ export default function PlayersList({ players, currentPlayer }) {
         players
           .filter((player) => currentPlayer?.id != player?.id)
           .map((player) => (
-            <li key={player.id}>
-              <img src={player.avatar} alt="" className="avatar" />
-              <span>
-                {player?.username}
-                {isVisble(player).toUpperCase()}
-              </span>
+            <li key={player.id} className="player">
+              <Player player={player} isVisble={isVisble} />
             </li>
           ))}
     </ul>
   );
 }
+
+const Player = ({ player, isVisble }) => {
+  return (
+    <>
+      <span>
+        <img src={player.avatar} alt="" className="avatar" />
+        {player?.username}
+      </span>
+      <span>{isVisble(player)}</span>
+    </>
+  );
+};
+
+const CurrentPlayer = ({ currentPlayer }) => {
+  const [hidden, setHidden] = useState(true);
+  return (
+    <>
+      <div className={`current-player-container ${hidden ? "show" : ""}`}>
+        {currentPlayer.ability && (
+          <img
+            src={`/char/${currentPlayer.ability}.webp`}
+            alt="loyal"
+            style={{ width: "128px", borderRadius: "0.5rem" }}
+          />
+        )}
+        {currentPlayer.role && (
+          <img
+            src={`/char/${currentPlayer.role}_symbol.webp`}
+            alt="loyal"
+            style={{ width: "128px" }}
+          />
+        )}
+      </div>
+      <span onClick={() => setHidden(!hidden)}>
+        {!hidden ? <FaEye size={32} /> : <FaEyeSlash size={32} />}
+      </span>
+    </>
+  );
+};
